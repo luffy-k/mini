dd3a98f8ca1eedfa34195c45a788a6b0850c615a
Jack <1376841224@qq.com>
2020-10-21 18:32:31 +0800

    first commit

---
+++ pages/demo/demo.js
@@ -0,0 +1,23 @@
+// demo.js
+
+Page({
+  data: {
+    title: 'Demo',
+    desc: ''
+  },
+  onLoad: function(options) {
+    // Do some initialize when page load.
+  },
+  onShow: function() {
+    // Do something when page show.
+  },
+  onReady: function() {
+    // Do something when page ready.
+  },
+  onHide: function() {
+    // Do something when page hide.
+  },
+  onUnload: function() {
+    // Do something when page close.
+  },
+});
---
+++ pages/tictactoe/tictactoe.js
@@ -0,0 +1,23 @@
+// tictactoe.js
+
+Page({
+  data: {
+    title: 'TicTacToe',
+    desc: ''
+  },
+  onLoad: function(options) {
+    // Do some initialize when page load.
+  },
+  onShow: function() {
+    // Do something when page show.
+  },
+  onReady: function() {
+    // Do something when page ready.
+  },
+  onHide: function() {
+    // Do something when page hide.
+  },
+  onUnload: function() {
+    // Do something when page close.
+  },
+});
