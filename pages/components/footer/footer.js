758f1484e745b31ec744280205ca1a5f47c23374
Jack <1376841224@qq.com>
2020-10-26 18:44:03 +0800

    update demo.js theme

--- pages/components/footer/footer.js
+++ pages/components/footer/footer.js
@@ -6,7 +6,22 @@ Component({
     addGlobalClass: true,
   },
   properties: {
-
+    showText: {
+      type: Boolean,
+      value: true
+    },
+    showTheme: {
+      type: Boolean,
+      value: true
+    },
+    isColumn:{
+      type: Boolean,
+      value: false
+    },
+    barBG: {
+      type: String,
+      value: '#fff'
+    },
   },
   data: {
     footerText: '',
@@ -16,12 +31,47 @@ Component({
     attached: function() {
       // 在组件实例进入页面节点树时执行
       const { footerText, theme } = app.globalData;
-      const icon = theme === 'light' ? 'night' : 'day';
-      const themeImg = `url(../../../static/images/weather-${icon}.png)`;
+      const themeImg = this.getThemeImg(theme);
       this.setData({footerText, themeImg});
     },
     detached: function() {
       // 在组件实例被从页面节点树移除时执行
     },
   },
+  methods: {
+    getThemeImg: function(theme){
+      const icon = theme === 'light' ? 'night' : 'day';
+      return `url(../../../static/images/weather-${icon}.png)`;
+    },
+    getTabBarStyle: function(theme){
+      return theme === 'light' ? {
+        backgroundColor: "#fff",
+        borderStyle: "black",
+      } : {
+        backgroundColor: '#111',
+        borderStyle: 'white'
+      };
+    },
+    getNavigationBarStyle: function(theme){
+      return theme === 'light' ? {
+        frontColor: '#000000',
+        backgroundColor: this.data.barBG,
+      } : {
+        frontColor: '#ffffff',
+        backgroundColor: '#111111',
+      }
+    },
+    themeClick: function(){
+      let {theme} = app.globalData;
+      theme = theme === 'light' ? 'dark' : 'light';
+      // 切换主题
+      app.themeChanged(theme);
+      // 切换图标navigationBar
+      this.setData({themeImg: this.getThemeImg(theme)});
+      // 切换顶部
+      wx.setNavigationBarColor(this.getNavigationBarStyle(theme));
+      // 切换底部tabBar
+      wx.setTabBarStyle(this.getTabBarStyle(theme));
+    }
+  }
 })
